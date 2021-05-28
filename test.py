902bbd2c2ad2fa06642755284d8ddc7e25d91139
0xredshadow <alblowy1@gmail.com>
2021-05-29 02:37:50 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+import random
+source = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+result = "".join(random.sample(source, 10))
+print(result)
+===========================
+>>>>>>>>>>>>>>>>>>> OUTPUT
+VCIPMJDYFL
+>>>>
+HYWSFZNBJX
+>>>>
+===========================
